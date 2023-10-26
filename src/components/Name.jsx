0b781e698ec795ef731610fc7282a7fa9451a8dd
psterner <psterner@hotmail.com>
2023-10-26 14:18:28 +0200

    Update classNames for styling

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,11 +1,18 @@
 export const Name = ({ value, updateFormData }) => {
+  const name = (e) => updateFormData("name", e.target.value);
 
-    const name = (e) => updateFormData("name", e.target.value);
-  
-    return (
-      <>
-        <label htmlFor="name">What is your name?</label>
-        <input type="text" name="name" value={value} onChange={name} placeholder="Type your name here please."/>
-      </>
-    );
-  };
+  return (
+    <div className="inputContainer">
+      <h2>What is your name?</h2>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={value}
+        onChange={name}
+        placeholder="Type your name here please."
+      />
+    </div>
+  );
+};
