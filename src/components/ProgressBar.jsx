a922d2dbda1923be8aa9f26d69eb6a6b53c215e7
psterner <psterner@hotmail.com>
2023-10-27 18:41:37 +0200

    Make progressbar more dynamic

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,22 +1,6 @@
 export const ProgressBar = ({ currentStep }) => {
-  // Select percentage depending on current step
-  let percentage = 0;
-  switch (currentStep) {
-    case 1:
-      percentage = 25;
-      break;
-    case 2:
-      percentage = 50;
-      break;
-    case 3:
-      percentage = 75;
-      break;
-    case 4:
-      percentage = 100;
-      break;
-    default:
-      percentage = 0;
-  }
+  // Making the progress bar dynamic, depending on the current step
+  let percentage = (currentStep - 1) * 25;
 
   return (
     <div className="progressContainer">
