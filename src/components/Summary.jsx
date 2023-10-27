70b2c727e71a9a4c96a2fd6ed7c0427ea885d386
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-10-27 15:33:17 +0200

    add styling to range slider

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -6,13 +6,11 @@ export const Summary = ({ formData }) => {
     <>
       <div className="summaryText">
         <p>
-          {" "}
-          Thanks <b>{name}</b>, we now know that you are in the age group{" "}
+          Thanks <b>{name}</b>, we now know that you are in the age group
           <b>{age} years old</b> and that you live in <b>{continent}</b>.
         </p>
         <p>
-          We then asked you <b>{questionText}</b> and you told us{" "}
-          <b>{subquestion}</b>, on a scale where <b>{labelText}</b>
+          We then asked you <b>{questionText}</b> and you told us <b>{subquestion}</b>, on a scale where <b>{labelText}</b>
         </p>
       </div>
     </>
