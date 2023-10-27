1ec6ca308a1490193d44791e59c16c37b500fbf6
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-10-27 09:46:26 +0200

    add styling to summary page + fix error in return info

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,11 +1,11 @@
 export const Summary = ({ formData }) => {
-  const { name, age, continent, questionText, value, labelText } = formData;
+  const { name, age, continent, questionText, subquestion, labelText } = formData;
 
   return (
     <>
-      <div>
-        <p> Thanks {name}, we now know that you are in the age group {age} years old and that you live in {continent}.</p>
-        <p>We then asked you {questionText} and you told us {value} on a scale where {labelText}</p>
+      <div className="summaryText">
+        <p> Thanks <b>{name}</b>, we now know that you are in the age group <b>{age} years old</b> and that you live in <b>{continent}</b>.</p>
+        <p>We then asked you <b>{questionText}</b> and you told us <b>{subquestion}</b>, on a scale where <b>{labelText}</b></p>
       </div>
     </>
   );
